18ea0476583110f0462a13d71b21974992f49e52
JXU2018 <jacky.qingxin.xu@gmail.com>
2018-02-05 10:12:31 +1300

    add save as draft

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,6 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
@@ -22,8 +21,19 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
+            if 'publish' in request.POST:
+                post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            request.session['last_post'] = "2016-2-2"
+            # if user.is_authenticated:
+                # print("user ok")
+                # request.session['name'] = user.username
+            print("session set")
+            print(request.session['last_post'])
+            # return redirect('post_detail', pk=post.pk)
+            # return redirect('post_list', pk=post.pk)
+            posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+            return render(request, 'blog/post_list.html', {'posts': posts})
     else:
         form = PostForm()
         return render(request, 'blog/post_edit.html', {'form': form})
